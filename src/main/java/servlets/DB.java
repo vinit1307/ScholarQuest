d749b8fcf45e6219fec3dc065bea34d1037c7f0b
vinit1307 <vinitmalwe48@gmail.com>
2025-03-24 17:58:08 +0530

    DB updated

--- src/main/java/servlets/DB.java
+++ src/main/java/servlets/DB.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 
 public class DB {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/scholarship?useSSL=false&autoReconnect=true&allowPublicKeyRetrieval=true";
+        String url = "jdbc:mysql://localhost:3306/scholarship?useSSL=false&autoReconnect=true&allowPublicKeyRetrieval=true&connectTimeout=30000";
         String user = "root";
         String password = "root";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, user, password);
+            Connection con = DriverManager.getConnection(url,user,password);
             System.out.println("Connection Successful!");
         } catch (Exception e) {
             e.printStackTrace();
