132d1652e936567bf1a824d2eba4585374863efa
vinit1307 <127872258+vinit1307@users.noreply.github.com>
2025-03-31 23:19:45 +0530

    DB Updated

--- DB/DbFile.sql
+++ DB/DbFile.sql
@@ -7,7 +7,11 @@ CREATE TABLE User (
     email VARCHAR(100) UNIQUE NOT NULL,
     category ENUM('General', 'OBC', 'SC', 'ST', 'Other') NOT NULL,
     annual_income ENUM('less than 1 lakh', '1 lakh - 3 lakh', '5 lakh - 7 lakh', '7 lakh and above') NOT NULL,
-    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    gender ENUM('Male', 'Female', 'Prefer not to say') NOT NULL,
+    high_school_percentage varchar(10) DEFAULT NULL,
+    higher_secondary_percentage varchar(10) DEFAULT NULL,
+    current_year ENUM('1st Year', '2nd Year', '3rd Year', '4th Year', 'Post Graduate') DEFAULT NULL,
+    cgpa DECIMAL(3,2) DEFAULT NULL,
     date_of_birth DATE NOT NULL,
     country VARCHAR(50) NOT NULL,
     state VARCHAR(50) NOT NULL,
@@ -21,13 +25,32 @@ CREATE TABLE ScholarshipData (
     name VARCHAR(255) NOT NULL,
     provider VARCHAR(255) NOT NULL,
     category ENUM('School Level', 'College Level', 'Government', 'International', 'For Girls') NOT NULL,
-    last_date varchar(50) NULL,  -- Can be NULL if not announced
-    amount DECIMAL(10,2) NOT NULL,
+    last_date varchar(200) NULL,  -- Can be NULL if not announced
+    amount varchar(200) NOT NULL,
+	age varchar(50) DEFAULT NULL,
+	annual_income varchar(50) DEFAULT NULL,
+    caste varchar(20) DEFAULT NULL,
     eligibility LONGTEXT NOT NULL,  -- Changed to store large paragraphs
     documents_required LONGTEXT NOT NULL,  -- Changed to store large paragraphs
     official_link VARCHAR(500) NOT NULL
 );
-use scholarship;
+
+select * from scholarshipdata;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* use scholarship;
 ALTER TABLE User 
 MODIFY COLUMN gender ENUM('Male', 'Female', 'Prefer not to say') NOT NULL,
 ADD COLUMN high_school_percentage DECIMAL(5,2) DEFAULT NULL AFTER gender,
@@ -52,7 +75,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
-
+*/
 
 
 
